e675a34c51fa6078730abc22f51539ec1f1d1b5a
ramdev.io.id@gmail.com <ramdev.io.id@gmail.com>
2022-04-26 06:09:05 +0700

    yok bisa

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import useScript from './hooks/useScript.js'
 import './App.css';
 
+import Sidebar from './components/Sidebar/sidebar.jsx'
+
 const App = () => {
   useScript('https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js', 'type', 'module')
   useScript('https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js', 'nomodule', '')
   
   return (
-    <div>
-      Hallo Code 
-      <ion-icon name="mail-outline"></ion-icon>
-    </div>
+    <main>
+      <Sidebar />
+    </main>
   );
 };
 
