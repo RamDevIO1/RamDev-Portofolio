d198b51b57406ffe29ef6818ff3fd1a8ba8d7442
ramdev.io.id@gmail.com <ramdev.io.id@gmail.com>
2022-04-26 06:21:19 +0700

    ...

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -5,6 +5,8 @@ import './sidebar.css';
 const Sidebar = () => {
 
   React.useEffect(() => {
+      const elementToggleFunc = function(elem) { elem.classList.toggle("active"); }
+
     // sidebar variables
     const sidebar = document.querySelector("[data-sidebar]");
     const sidebarBtn = document.querySelector("[data-sidebar-btn]");
