88d4338310e08f81e3ec7d9c08f1b82db2fab96c
ramdev.io.id@gmail.com <ramdev.io.id@gmail.com>
2022-04-27 12:08:55 +0700

    .

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "Coming Soon",
     category: "none",
-    image: "../../assets/images/coming-soon.png",
+    image: "../../assets/images/project-8.jpg",
     link: ""
   },
   {
